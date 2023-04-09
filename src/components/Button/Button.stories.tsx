c360b74826e0647ac3dcfc000f9d71117aed3474
med <med.elkhalki0@gmail.com>
2023-04-09 22:25:14 +0200

    Adding Storybook

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+import Button from "./Button";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "ReactComponentLibrary/Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const HelloWorld = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+HelloWorld.args = {
+  label: "Save",
+};
+
+export const ClickMe = Template.bind({});
+ClickMe.args = {
+  label: "Click me!",
+};
